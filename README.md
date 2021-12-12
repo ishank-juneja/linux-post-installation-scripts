dae9daa3c9945fc7ebd4ac4557e9d7ac48cb0e98
Ishank Juneja <ishankjuneja@gmail.com>
2021-12-11 23:33:32 -0500

    Added basic install script init version

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Post installation scripts to set up work environment on a new computer system. 
 
+# Usage
+Linux work environment setup
+```
+git clone https://github.com/ishank-juneja/setup-scripts.git setup_scripts
+cd setup_scripts
+chmod +x ubuntu-post-installer.sh 
+sudo ./ubuntu-post-installer.sh
+```
--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+# apt programs
+sudo apt update
+# AnyDesk
+sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+# Update before final install since repo added
+sudo apt update
+sudo apt install anydesk
+# TexStudio
+sudo apt install texlive-full
+sudo apt-get install texstudio
+# Git
+sudo apt install git-all
+# Open-shot video editor
+sudo add-apt-repository ppa:openshot.developers/ppa
+sudo apt update
+sudo apt install openshot-qt python3-openshot
+
 # Snap Install
+sudo apt install snapd
 # Slack
 sudo snap install slack --classic
-
+# Pycharm
+sudo snap install pycharm-professional --classic
+# VLC player
+sudo snap install vlc
 
 # Install the usual programs
 sudo apt  -y
